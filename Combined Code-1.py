bcf4e0cc0c8fa07b3fed05bdd8f6f229b4440b09
Zabro <reinzabrocki@shaw.ca>
2023-04-15 21:52:52 -0600

    Light Sensor update it works

--- Combined Code-1.py
+++ Combined Code-1.py
@@ -1,4 +1,3 @@
-
 #Add Phidgets Library
 from Phidget22.Phidget import *
 from Phidget22.Devices.Accelerometer import *
@@ -8,7 +7,7 @@ import time
 import array 
 import numpy as np
 
-# remeber to comment lots 
+# remember to comment lots 
 
 
 LightSamples = 25
@@ -17,13 +16,12 @@ SampleSum =  0
 LightCoe = 40
 MinTimeDifference = 0.001
 
-#Create
 #accelerometer = Accelerometer()
 lightSensor = LightSensor()
-
-#Open
 #accelerometer.openWaitForAttachment(1000)
 lightSensor.openWaitForAttachment(1000)
+
+
 
 #Calculatus the period using time it takes for the light sensor to see the light attached to the arm
 def period(BrightnessRegistor):
@@ -41,12 +39,12 @@ def period(BrightnessRegistor):
             #print("Time2 =", Time2)
             #print("break")
             break
-        
-        
+           
     Period = (Time2 - Time1)
     #Period = time over cycles 
     return Period
 
+<<<<<<< Updated upstream
 def cleanprint(Period,MinTimeDifference):
     if(Period < MinTimeDifference):
         return
@@ -56,17 +54,40 @@ def cleanprint(Period,MinTimeDifference):
     
     
 
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 
 SD = int(input("How many decimal places do you want to round to?"))
 print("put anything to start the loops")
 a = input()
+=======
+def cleanprint(Period,MinTimeDifference):
+    if(Period < MinTimeDifference):
+        return
+    print("this is the period,", Period)
+    return
+
+#will use later
+def CSVwrite():
+    return
+
+    
+    
+
+## main program starts here ########################################################
+SD = int(input("How many decimal places do you want to round to? "))
+input("put anything to start the loops. ")
+>>>>>>> Stashed changes
 print("Light sensor calibration starting", LightSamples, "light samples will be taken" )
+print("Loading...")
 while(i < LightSamples):
     i = i + 1
     print(i)
 print("done loop")
 i = 0
 print("Do not change lighting")
+
 while (i < LightSamples):
     #SampleSum =+ lightSensor.getIlluminance()
     print(lightSensor.getIlluminance())
@@ -74,6 +95,7 @@ while (i < LightSamples):
     time.sleep(0.2)
     i = i + 1
     print("Cycles =", i)
+    
     if i == LightSamples:
         #when the amount of times ran is over the brightness to registor a period is callculated
         #the average brightness is also calculated
@@ -85,12 +107,12 @@ while (i < LightSamples):
         break
 
 
-print("out of the loop now ahh")
+input("Input anything to start the main program. ")
 
-print("input anything to start the main program")
-a = input()
 while(True):
     Period = period(BrightnessRegistor)
     cleanprint(Period,MinTimeDifference)
+    
+    
     
     
