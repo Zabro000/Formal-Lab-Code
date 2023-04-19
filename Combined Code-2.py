8cbbab072310589c2839f95c1f6df8186832646c
Zabro <reinzabrocki@shaw.ca>
2023-04-19 17:33:57 -0600

    Code clean up  1

--- Combined Code-2.py
+++ Combined Code-2.py
@@ -2,84 +2,53 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.LightSensor import *
 from Phidget22.Devices.DCMotor import *
-#Required for sleep statement
 import time
 import array 
 import numpy as np
 import csv
 
-# remember to comment lots 
-
-
-LightSamples = 25
-i = 0
-SampleSum =  0
-LightCoe = -40
-MinTimeDifference = 0.05
 lightSensor = LightSensor()
 lightSensor.openWaitForAttachment(1000)
+
+
+#Varribles for code
+LightCoe = -40
+MinTimeDifference = 0.05
 PeriodTolerance = 0.10
 
-
+#Varribles for calculations
 #in meters and in kilograms, change these
 ArmRadius = 0.225
 ObjectMass = 0.01
 
-# def motorstart():
-#     dcMotor0 = DCMotor()
-# 	dcMotor1 = DCMotor()
-# 
-# 	dcMotor0.setHubPort(5)
-# 	dcMotor0.setChannel(0)
-# 	dcMotor1.setHubPort(5)
-# 	dcMotor1.setChannel(1)
-# 
-# 	dcMotor0.openWaitForAttachment(5000)
-# 	dcMotor1.openWaitForAttachment(5000)
-# 
-# 	dcMotor0.setTargetVelocity(1)
-# 	dcMotor1.setTargetVelocity(1)
-# 
-# # 	dcMotor0.close()
-# # 	dcMotor1.close()
-
-    
-
+#light sample varribles
+LightSamples = 30
+i = 0
+SampleSum =  0
 
 #Calculatus the period using time it takes for the light sensor to see the light attached to the arm
 def period(BrightnessRegistor,MinTimeDifference,LoopTimeStart):
-    Time1 = time.perf_counter() # doesnt change in while loop STAR
+    Time1 = time.perf_counter() 
     Time2 = 0
     Period = 0
     while(True):
           CurrentLightValue = lightSensor.getIlluminance()
           time.sleep(0.01)
-          #when brightness is lowered it jumps back causing same effect
-          #print(CurrentLightValue)
+          
+          #Measures when brightness goes down as a result of the black paper
           if(CurrentLightValue < BrightnessRegistor):
               Time2 = time.perf_counter()
               Period = (Time2 - Time1)
               return Period
           
-          
-def smallperiod():
-    Time2 = time.perf_counter()
-
-
-# def cleanprint(Period,MinTimeDifference):
-#     if(Period < MinTimeDifference):
-#         return
-#     print("this is the period,", Period)
-#     return
-    
+#opens or creates the csv file       
 def CSVwriteStart():
     print("Opening the data file...")
     with open ('Lab_data.csv','w') as datafile:
           write = csv.writer
              
-
+#writes all data to a csv file for later use in graphing
 def CSVwrite(Period,ArmRadius,ObjectMass):
-    
     
     VelocityC = (2*(3.1415)*ArmRadius)/Period
     AccelC = (VelocityC * VelocityC)/ArmRadius
@@ -96,7 +65,7 @@ def CSVwrite(Period,ArmRadius,ObjectMass):
 
 
 
-## main program starts here ########################################################
+## main program starts here ##########################################################################################
 print("Soleniods are being attached...")
 dcMotor0 = DCMotor()
 dcMotor1 = DCMotor()
@@ -120,11 +89,15 @@ print("Do not change the lighting of the room")
 
 time.sleep(3)
 
+LightSamples = 30
+i = 0
+SampleSum =  0
+
+#finds the average ambient brightness of the room over a number of samples
 while (i < LightSamples):
-    #SampleSum =+ lightSensor.getIlluminance()
     print(lightSensor.getIlluminance())
     SampleSum = SampleSum + lightSensor.getIlluminance()
-    time.sleep(0.2)
+    time.sleep(0.3)
     i = i + 1
     print("Cycles =", i)
     
@@ -141,6 +114,7 @@ while (i < LightSamples):
 
 input("Input anything to start the main program. ")
 
+#Varribles for main loop are created
 i = 0
 ProgramCheck = 40
 Break = 0
@@ -150,6 +124,8 @@ PeriodFinal = 0
 while(Break == 0):
     LoopTimeStart = time.perf_counter()
    
+   #finds the period between each change in brightness
+   #if and else statment top changes from reistering if the brightness is constantly different
     while(True):
         Period = period(BrightnessRegistor,MinTimeDifference,LoopTimeStart)
         if(Period < MinTimeDifference):
@@ -157,15 +133,15 @@ while(Break == 0):
         else:
             break
     
-#    print(" Real Period is ", RealPeriod)
-    print("Current period is: ", round(Period,SD))
-    #cleanprint(Period,MinTimeDifference)
+
+    print("\n", "Current period is: ", round(Period,SD))
     PeriodFinal = Period
     print("Inital period is:", round(PeriodInital,SD))
     
     ChangeInPeriod = PeriodFinal - PeriodInital
     
     PeriodInital = PeriodFinal
+    
     print("Final period is: ", round(PeriodFinal,SD))
     print("The change in period is: ", round(ChangeInPeriod,SD))
     
@@ -173,27 +149,32 @@ while(Break == 0):
     print("loop count ", i)
     print(" ")
     
+    #Releases the solenoid if the change in the period is minimal, that is uniform circluar motion. 
     if(-PeriodTolerance < ChangeInPeriod < PeriodTolerance):
         input("Type in anything to release the Solenoids: ")
         while(True):
+            #the next time the light sensor is hit it releases the solenoid pins
             CurrentLightValue = lightSensor.getIlluminance()
             time.sleep(0.02)
             if(CurrentLightValue < BrightnessRegistor):
+                
                 #changed signs for the black paper
                 #Relase Solenoids and record Period, VC
                 dcMotor0.setTargetVelocity(0)
                 dcMotor1.setTargetVelocity(0)
                 CSVwrite(Period, ObjectMass, ArmRadius)
+                
                 print("Stop the motor to reset expariment and unplug the solen oids to cool them down.")
                 print(" ")
-                input("Input anything to continue the program")
+                input("Input anything to continue the program: ")
                 print(" ")
+                
                 dcMotor0.setTargetVelocity(1)
                 dcMotor1.setTargetVelocity(1)
                 break
             
         
-    # After some amount of loops this program runs it asks if more observations
+    #After some amount of loops this program runs it asks if more observations
     #Still want to be done, using i varbile to count times
     
     if(i >=  ProgramCheck):
