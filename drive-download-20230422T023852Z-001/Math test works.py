864cfc76994653cc3d5503c944995235111ed0d8
Zabro <reinzabrocki@shaw.ca>
2023-04-21 20:40:16 -0600

    Arpil 21 Download

---
+++ drive-download-20230422T023852Z-001/Math test works.py
@@ -0,0 +1,16 @@
+Period = 0.2679842
+Radius = 0.225
+mass = 0.0008
+
+
+def main(Period, Radius, mass):
+    Vc = (2 * 3.1415 * Radius)/Period
+    print(Vc)
+
+    Ac = (Vc * Vc)/Radius
+    print(Ac)
+
+    Ft = mass * Ac
+    print(Ft)
+
+main(Period, Radius, mass)
